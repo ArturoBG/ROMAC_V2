a4016e567683012f3afd8fbb8eb1492ce815007c
Arturo Bringas <artur.aesir@gmail.com>
2023-11-18 11:06:25 -0600

    Clase 17
    UI MainMenu
    LoadLevel  w/ Scene Manager

--- Assets/Scripts/Misc/MainMenu.cs
+++ Assets/Scripts/Misc/MainMenu.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-    public TextMesh startGameText;
     public Color white;
     public Color black;
-
+    public Animator UIAnimator;
 
     public void ChangeTextColorWhite(TMP_Text textMesh)
     {
@@ -16,5 +17,46 @@ public class MainMenu : MonoBehaviour
     public void ChangeTextColorBlack(TMP_Text textMesh)
     {
         textMesh.color = black;
+    }
+
+    public void NewGame()
+    {
+        UIAnimator.SetTrigger("newgame");
+    }
+
+    public void GotoSettings()
+    {
+        UIAnimator.SetBool("settings", true);
+    }
+
+    public void ExitGame()
+    {
+        UIAnimator.SetBool("exit", true);
+    }
+
+    public void BackSettings()
+    {
+        UIAnimator.SetBool("settings", false);
+    }
+
+    public void BackExit()
+    {
+        UIAnimator.SetBool("exit", false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();    
+    }
+
+    public void LoadGame()
+    {
+        StartCoroutine(LoadGameRoutine());
+    }
+
+    IEnumerator LoadGameRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("PlayerSandbox");
     }
 }
